5480e74fbf82d6d648842f37216fb900421817a1
Henrique Valentim Nunes <henriquevalentim16@gmail.com>
2023-04-07 14:42:50 -0300

    feat: adjust carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,22 +1,32 @@
 import { Carousel as Slider } from 'react-responsive-carousel'
 import { ContainerImage } from './styles'
+import { useEffect, useState } from 'react'
+import api from '../../utils/api'
 
 export default function Carousel() {
+  const [carousel, setCarousel] = useState()
+
+  useEffect(() => {
+    const getCarrousel = async () => {
+      const response = await api.get(`/setting/SETTING_CAROUSEL_01`)
+      setCarousel(response.data.metadatas)
+    }
+
+    getCarrousel()
+  }, [])
+  if (!carousel) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div>
       <Slider showThumbs={false}>
-        <ContainerImage>
-          <img src='https://via.placeholder.com/400x200' alt='Carousel' />
-          <p className='legend'>Legend 1</p>
-        </ContainerImage>
-        <ContainerImage>
-          <img src='https://via.placeholder.com/400x200' alt='Carousel' />
-          <p className='legend'>Legend 1</p>
-        </ContainerImage>
-        <ContainerImage>
-          <img src='https://via.placeholder.com/400x200' alt='Carousel' />
-          <p className='legend'>Legend 1</p>
-        </ContainerImage>
+        {carousel.map((item) => (
+          <ContainerImage>
+            <img src={item.key} alt={item.value} />
+            <p className='legend'>{item.value}</p>
+          </ContainerImage>
+        ))}
       </Slider>
     </div>
   )
